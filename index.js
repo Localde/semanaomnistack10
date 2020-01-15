5b341875b01ea0d8df589ebb7b28b6798b91710c
Ollywer <dyegoollywer@gmail.com>
2020-01-14 23:41:25 -0300

    Testando método delete

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const app = express();
 
-//get, post, put, delete
+// Métodos HTTP: get, post, put, delete # Métodos semânticas.
 
-app.get('/', (request, response) => {
+// Tipos de parâmetros: 
+
+// Query Params: request.query (Filtros, ordemação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: 
+
+app.delete('/users/:id', (request, response) => {
+    console.log(request.params);
     return response.json({ message: 'Hello OmniStack' });
 });
 
