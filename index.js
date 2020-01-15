9eb2acac70c50187a88808e23d40c00213b4dcd1
Ollywer <dyegoollywer@gmail.com>
2020-01-14 22:47:15 -0300

    Comentario sobre métodos do express()

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+//get, post, put, delete
+
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello OmniStack' });
 });
